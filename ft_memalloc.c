35f5e7b7a146fd2c50c9f70b441f5dd71e36b6af
Yann VILLEPONTOUX <yvillepo@e3r10p11.42.fr>
2017-12-06 18:58:22 +0100

    memaloc exit si echou

--- ft_memalloc.c
+++ ft_memalloc.c
@@ -6,7 +6,7 @@
 /*   By: yvillepo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/10 17:25:09 by yvillepo          #+#    #+#             */
-/*   Updated: 2017/11/14 20:00:39 by yvillepo         ###   ########.fr       */
+/*   Updated: 2017/12/06 18:57:47 by yvillepo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void	*ft_memalloc(size_t size)
 	void	*mem;
 
 	if (!(mem = malloc(size)))
-		return (NULL);
+		exit_error("memalloc");
 	ft_bzero(mem, size);
 	return (mem);
 }
